6d3fef9211f4d966b86ba382a85cbdb169116be1
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2025-11-13 14:05:20 +0100

    checking stuff

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -1,7 +1,9 @@
-import * as Sentry from '@sentry/tanstackstart-react'
+// import * as Sentry from '@sentry/tanstackstart-react'
 
-Sentry.init({
-  dsn: 'https://f891c24557d428e5945c2a2889a15d49@o4507958847078400.ingest.us.sentry.io/4507963537424384',
-  sendDefaultPii: true,
-  // tunnel: '/tunnel',
-})
+// Sentry.init({
+//   dsn: 'https://f891c24557d428e5945c2a2889a15d49@o4507958847078400.ingest.us.sentry.io/4507963537424384',
+//   sendDefaultPii: true,
+//   // tunnel: '/tunnel',
+// })
+
+console.log('No install needed.')
