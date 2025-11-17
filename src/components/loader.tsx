431a55493868af325b071871c995175a2037eaa7
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2025-11-17 19:00:30 +0100

    fix

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,17 +1,25 @@
-import { useLottie, LottieOptions } from 'lottie-react'
-import LoadingAnimation from '@/assets/lottie/loading.json'
+// import { useLottie, LottieOptions } from 'lottie-react'
+// import LoadingAnimation from '@/assets/lottie/loading.json'
+
+// export const Loader = () => {
+//   const options: LottieOptions = {
+//     animationData: LoadingAnimation,
+//     loop: true,
+//     className: 'h-40 w-40 md:h-60 md:w-60',
+//   }
+//   const { View } = useLottie(options)
+
+//   return (
+//     <div className="flex h-screen w-screen items-center justify-center">
+//       <div className="h-40 w-40 md:h-60 md:w-60">{View}</div>
+//     </div>
+//   )
+// }
 
 export const Loader = () => {
-  const options: LottieOptions = {
-    animationData: LoadingAnimation,
-    loop: true,
-    className: 'h-40 w-40 md:h-60 md:w-60',
-  }
-  const { View } = useLottie(options)
-
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <div className="h-40 w-40 md:h-60 md:w-60">{View}</div>
+      <div className="h-40 w-40 md:h-60 md:w-60">Loading...</div>
     </div>
   )
 }
